eb2e92609c5a417fb676751aa6d079fc53530ff4
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2017-12-20 13:46:23 +0100

    archive_eccodes_data.sh download all data from ECMWF

--- archive_eccodes_data.sh
+++ archive_eccodes_data.sh
@@ -8,30 +8,9 @@ cleanup()
     [[ -n "$workdir" ]] && rm -rf $workdir
 }
 
-version=$1
-outdir=$PWD
-pkgfile=eccodes-${version}-Source.tar.gz
-pkgurl="https://software.ecmwf.int/wiki/download/attachments/45757960/${pkgfile}?api=v2"
-dataurl="http://download.ecmwf.org/test-data/grib_api/data/"
-workdir=$(mktemp -d)
-listfile=$workdir/list
-
-pushd $workdir
-wget --no-verbose -O $pkgfile $pkgurl
-tar xvvf $pkgfile
-pushd ${pkgfile%.tar.gz}
-
-pushd data
-while read l
-do
-    d=$(dirname $l | sed -e 's,^\./,,g')
-    sed -e "s,^,$dataurl/$d/,g" < $l >> $listfile
-done < <(find . -name "*_files.txt")
-popd
-
-popd
-
-xargs -d '\n' wget --no-verbose -x -nH --cut-dirs=3 --directory data < $listfile
+outdir=$(readlink -f .)
+pushd workdir
+wget --recursive -nH --cut-dirs=3 --no-parent --directory-prefix=data http://download.ecmwf.org/test-data/grib_api/data/
 
 pushd data
 tar cvvfz $outdir/eccodes-data.tar.gz *
